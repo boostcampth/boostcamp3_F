f8afa5ec73d7f3616fc6d66f174272aa978ec55d
onedelay <wjy0218@gmail.com>
2019-01-27 22:35:28 +0900

    뷰타입 추가 및 그룹핑

--- app/src/main/java/com/boostcamp/travery/main/adapter/RouteListAdapter.kt
+++ app/src/main/java/com/boostcamp/travery/main/adapter/RouteListAdapter.kt
@@ -6,26 +6,49 @@ import android.view.ViewGroup
 import com.boostcamp.travery.OnItemClickListener
 import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseAdapter
-import com.boostcamp.travery.data.model.Route
-import com.boostcamp.travery.databinding.ItemRouteListBinding
 import com.boostcamp.travery.base.BaseViewHolder
+import com.boostcamp.travery.databinding.ItemRouteListBinding
+import com.boostcamp.travery.databinding.ItemRouteListGroupBinding
+import com.boostcamp.travery.main.viewholder.GroupItem
+import com.boostcamp.travery.main.viewholder.GroupViewHolder
 import com.boostcamp.travery.main.viewholder.RouteViewHolder
 
-class RouteListAdapter(private var listener: OnItemClickListener) : BaseAdapter<Route>() {
+class RouteListAdapter(private var listener: OnItemClickListener) : BaseAdapter<Any>() {
+
+    companion object {
+        private const val VIEW_TYPE_GROUP = 1
+        private const val VIEW_TYPE_ITEM = 2
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-        DataBindingUtil.inflate<ItemRouteListBinding>(
-            LayoutInflater.from(parent.context),
-            R.layout.item_route_list,
-            parent,
-            false
-        ).also {
-            it.listener = listener
-            return RouteViewHolder(it)
+        return when (viewType) {
+            VIEW_TYPE_GROUP -> {
+                val binding = DataBindingUtil.inflate<ItemRouteListGroupBinding>(
+                        LayoutInflater.from(parent.context),
+                        R.layout.item_route_list_group,
+                        parent,
+                        false)
+                GroupViewHolder(binding)
+            }
+
+            else -> {
+                val binding = DataBindingUtil.inflate<ItemRouteListBinding>(
+                        LayoutInflater.from(parent.context),
+                        R.layout.item_route_list,
+                        parent,
+                        false)
+                binding.listener = this@RouteListAdapter.listener
+                RouteViewHolder(binding)
+            }
         }
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.bind(data[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (data[position] is GroupItem) VIEW_TYPE_GROUP
+        else VIEW_TYPE_ITEM
     }
 }
