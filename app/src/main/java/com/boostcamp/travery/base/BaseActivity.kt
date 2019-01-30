e919b96cb1fcccdeca311c4754d4786c5d76947f
onedelay <wjy0218@gmail.com>
2019-01-30 23:53:48 +0900

    (#38) Progress bar 수정

--- app/src/main/java/com/boostcamp/travery/base/BaseActivity.kt
+++ app/src/main/java/com/boostcamp/travery/base/BaseActivity.kt
@@ -1,48 +1,55 @@
 package com.boostcamp.travery.base
 
-import android.app.Activity
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDialog
 import com.boostcamp.travery.R
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.progressbar_loading.*
 
 
 open class BaseActivity : AppCompatActivity() {
-
-    companion object {
-        var progressDialog: AppCompatDialog? = null
-    }
+    var progressDialog: AppCompatDialog? = null
 
     // usage : "토스트".toast
     fun Any.toast(duration: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(baseContext, this.toString(), duration).show()
     }
 
-    fun progressON(activity: Activity?, message: String?) {
-        if (activity?.isFinishing != false) {
+    fun progressOn(message: String?) {
+        if (this.isFinishing) {
             return
         }
         if (progressDialog?.isShowing == true) {
-            progressSET(message)
+            progressSet(message)
         } else {
-            progressDialog = AppCompatDialog(activity)
-                .apply {
-                setCancelable(false)
-                setContentView(R.layout.progressbar_loading)
-                show()
-            }
+            progressDialog = AppCompatDialog(this)
+                    .apply {
+                        setCancelable(false)
+                        setContentView(R.layout.progressbar_loading)
+                        show()
+                    }
+        }
+
+        progressDialog?.let {
+            it.findViewById<TextView>(R.id.tv_pregress_message)?.text = message
+            val imageView = it.findViewById<ImageView>(R.id.iv_frame_loading) ?: return
+            Glide.with(this)
+                    .load(R.drawable.progress_bar)
+                    .into(imageView)
         }
     }
 
-    private fun progressSET(message: String?) {
+    private fun progressSet(message: String?) {
         if (progressDialog?.isShowing != false) {
             return
         }
         tv_pregress_message.text = message ?: " "
     }
 
-    fun progressOFF() {
+    fun progressOff() {
         if (progressDialog?.isShowing != false) {
             progressDialog?.dismiss()
         }
