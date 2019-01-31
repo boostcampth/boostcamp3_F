82cc82fe6534725b4f99cf4d970dd092ccd8bedf
park-ju1008 <p7343703@gmail.com>
2019-01-31 11:07:05 +0900

    (#40) Room Dao 구현

--- app/src/main/java/com/boostcamp/travery/data/local/db/dao/CourseDao.kt
+++ app/src/main/java/com/boostcamp/travery/data/local/db/dao/CourseDao.kt
@@ -1,30 +1,27 @@
 package com.boostcamp.travery.data.local.db.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
 import com.boostcamp.travery.data.model.Course
-import io.reactivex.Flowable
 
 @Dao
 interface CourseDao {
     @Insert
-    fun insertCourse(course: Course): Long
-
-    @Update
-    fun updateCourse(course: Course)
+    fun insert(course: Course)
 
     @Delete
-    fun deleteCourse(course: Course)
+    fun delete(course: Course)
+
+    @Delete
+    fun delete(courseList: List<Course>)
 
     /**
-     * 저장된 경로 리스트 반환
-     *
-     * @param index 시작 인덱스
-     * @param count 가져올 course 갯수
-     *
-     * @return 저장된 경로 중 지정 인덱스부터 count 만큼  반환
+     * 저장된 모든 경로 리스트 반환
      */
-    @Query("SELECT * FROM course ORDER BY end_time DESC LIMIT :index,:count ")
-    fun loadCourseList(index: Int, count: Int): Flowable<List<Course>>
+    @Query("SELECT * FROM course")
+    fun loadAll(): List<Course>
 
     /**
      * title 또는 theme 에 해당 키워드가 포함된 경로검색
@@ -35,6 +32,6 @@ interface CourseDao {
      *
      * @return 해당 키워드로 검색된 row 중 지정 인덱스부터 count 만큼  반환
      */
-    @Query("SELECT * FROM course WHERE title LIKE '%'||:keyword||'%' OR theme LIKE '%'||:keyword||'%' LIMIT :index,:count")
-    fun searchCourseForKeyword(keyword: String, index: Int, count: Int): Flowable<List<Course>>
+    @Query("SELECT * FROM course WHERE title LIKE '%'||:keyword||'%' OR theme LIKE '%'||:keyword||'%'")
+    fun searchCourseForKeyword(keyword: String): List<Course>
 }
--- app/src/main/java/com/boostcamp/travery/data/local/db/dao/UserActionDao.kt
+++ app/src/main/java/com/boostcamp/travery/data/local/db/dao/UserActionDao.kt
@@ -2,26 +2,44 @@ package com.boostcamp.travery.data.local.db.dao
 
 import androidx.room.*
 import com.boostcamp.travery.data.model.UserAction
-import io.reactivex.Flowable
 
 @Dao
 interface UserActionDao {
     @Insert
-    fun insertActivity(activity: UserAction)
+    fun insert(userAction: UserAction)
+
+    @Insert
+    fun insert(userActionList: List<UserAction>)
 
     @Update
-    fun updateActivity(activity: UserAction)
+    fun update(userAction: UserAction)
 
     @Delete
-    fun deleteActivity(activity: UserAction)
+    fun delete(userAction: UserAction)
+
+    @Delete
+    fun delete(userActionList: List<UserAction>)
+
+    @Query("SELECT * FROM useraction")
+    fun loadAll(): List<UserAction>
 
     /**
      * 해당 경로에 속하는 활동들을 반환
      *
-     * @param courseCode 경로 코드
+     * @param courseKey 경로 고유 키 값
      *
      * @return 활동 목록
      */
-    @Query("SELECT * FROM useraction WHERE course_code=:courseCode")
-    fun getActivitList(courseCode:Int): Flowable<List<UserAction>>
+    @Query("SELECT * FROM useraction WHERE course_code=:courseKey")
+    fun loadUserActionForCourse(courseKey: Long): List<UserAction>
+
+    /**
+     * 해당 keyword를 title,body,hashtag에 포함한 활동들을 반환
+     *
+     * @param courseKey 경로 고유 키 값
+     *
+     * @return 활동 목록
+     */
+    @Query("SELECT * FROM useraction WHERE title LIKE '%'||:keyword||'%' OR body LIKE '%'||:keyword||'%' OR hashtag LIKE '%'||:keyword||'%'")
+    fun searchUserActionForKeyword(keyword: String): List<UserAction>
 }
