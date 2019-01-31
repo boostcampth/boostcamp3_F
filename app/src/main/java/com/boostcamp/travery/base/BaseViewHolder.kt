3e21958c4f11f63ca3d08c451d6f2531986e1c9e
onedelay <wjy0218@gmail.com>
2019-02-01 01:09:40 +0900

    BaseAdapter 수정

--- app/src/main/java/com/boostcamp/travery/base/BaseViewHolder.kt
+++ app/src/main/java/com/boostcamp/travery/base/BaseViewHolder.kt
@@ -4,6 +4,6 @@ import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseViewHolder(binding: ViewDataBinding) :
-    androidx.recyclerview.widget.RecyclerView.ViewHolder(binding.root) {
+    RecyclerView.ViewHolder(binding.root) {
     abstract fun bind(item: Any)
 }
---
+++ app/src/main/java/com/boostcamp/travery/base/OnClickListeners.kt
@@ -0,0 +1,9 @@
+package com.boostcamp.travery.base
+
+interface OnItemClickListener {
+    fun onItemClick(position: Int)
+}
+
+interface OnItemLongClickListener {
+    fun onItemLongClick(position: Int)
+}
