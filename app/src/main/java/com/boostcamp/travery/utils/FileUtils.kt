2d45b52c86fab32f94c7f1b20503c7fa127ed940
MYUNG-PC\Myung <myung6024@naver.com>
2019-02-01 15:04:38 +0900

    (#47) FileUtils 코스 파일 삭제 기능 추가

--- app/src/main/java/com/boostcamp/travery/utils/FileUtils.kt
+++ app/src/main/java/com/boostcamp/travery/utils/FileUtils.kt
@@ -4,28 +4,35 @@ import android.content.Context
 import android.graphics.Bitmap
 import org.json.JSONObject
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 
 object FileUtils {
-    fun saveJsonFile(context: Context, dirName: String, fileName: String, jsonObj : JSONObject){
-        val fullName = if(!dirName.isEmpty()) "$dirName/$fileName" else fileName
+    fun saveJsonFile(context: Context, fileName: String, jsonObj: JSONObject) {
         context.openFileOutput("$fileName.json", Context.MODE_PRIVATE).use {
             it.write(jsonObj.toString().toByteArray())
         }
     }
 
-    fun saveImageFile(context: Context, dirName: String, fileName: String, bitmap : Bitmap){
-        val fullName = if(!dirName.isEmpty()) "$dirName/$fileName" else fileName
+    fun saveImageFile(context: Context, fileName: String, bitmap: Bitmap) {
         context.openFileOutput("$fileName.jpg", Context.MODE_PRIVATE).use {
             it.write(bitmap.ninePatchChunk)
         }
     }
 
-    fun loadStringFromJsonFile(context: Context, fileName: String): String{
+    fun loadStringFromJsonFile(context: Context, fileName: String): String {
         val directory = context.filesDir
         val file = File(directory, "$fileName.json")
 
         return file.readText()
+    }
+
+    fun deleteCourseFile(context: Context, fileName: String) {
+        val directory = context.filesDir
+        val fileList = File(directory, "").listFiles()
+
+        for (i in 0 until fileList.size) {
+            if (fileList[i].name.contains(fileName)) {
+                fileList[i].delete()
+            }
+        }
     }
 }
