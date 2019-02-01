5a62ec4318d15c176cf018a73e081ed4663cd860
MYUNG-PC\Myung <myung6024@naver.com>
2019-02-01 16:53:14 +0900

    (#54) CourseSave 테마 관련 데이터 바인딩 버그 수정

--- app/src/main/java/com/boostcamp/travery/mapservice/savecourse/CourseSaveViewModel.kt
+++ app/src/main/java/com/boostcamp/travery/mapservice/savecourse/CourseSaveViewModel.kt
@@ -9,6 +9,7 @@ import com.boostcamp.travery.base.BaseViewModel
 import android.widget.AdapterView
 import androidx.databinding.ObservableField
 import com.boostcamp.travery.Constants
+import com.boostcamp.travery.R
 import com.boostcamp.travery.data.AppDataManager
 import com.boostcamp.travery.data.local.db.AppDatabase_Impl
 import com.boostcamp.travery.data.local.db.AppDbHelper
@@ -30,13 +31,18 @@ class CourseSaveViewModel(application: Application) : BaseViewModel(application)
     private var body: String = ""
         get() = if (field.isEmpty()) "활동" else field
 
+    private var inputTheme: String = ""
+        get() = if (field.isEmpty()) "활동" else field
+
     val theme = ObservableField<String>("")
 
     private fun makeCoordinateJson(locationList: ArrayList<LatLng>, timeList: ArrayList<String>): JSONObject {
 
+        //path=color:0x0000ff|weight:5|40.737102,-73.990318|40.749825,-73.987963|40.752946,-73.987384|40.755823,-73.986397
         val coordinates = JSONObject()
         val coordinateItem = JSONArray()
         val colms = JSONObject()
+        var urlPath:StringBuffer
         for (i in 0..(locationList.size - 1)) {
             val coordinate = JSONObject()
             coordinate.put("lat", locationList[i].latitude)
@@ -69,7 +75,14 @@ class CourseSaveViewModel(application: Application) : BaseViewModel(application)
                     Course(
                         title,
                         body,
-                        theme.get(),
+                        when (theme.get()) {
+                            getApplication<Application>().getString(R.string.string_input_theme) -> {
+                                inputTheme
+                            }
+                            else -> {
+                                theme.get()
+                            }
+                        },
                         mCourse!!.startTime,
                         mCourse.endTime,
                         mCourse.distance,
@@ -99,5 +112,9 @@ class CourseSaveViewModel(application: Application) : BaseViewModel(application)
 
     fun onBodyChange(body: CharSequence) {
         this.body = body.toString()
+    }
+
+    fun onThemeChange(theme: CharSequence) {
+        this.inputTheme = theme.toString()
     }
 }
