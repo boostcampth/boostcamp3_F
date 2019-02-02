2fea529ab3ace8653900e8895f0f8b5dc96b41b8
park-ju1008 <p7343703@gmail.com>
2019-02-03 01:43:07 +0900

    (#52) 좌표정보로 폴리라인 생성 구현

--- app/src/main/java/com/boostcamp/travery/coursedetail/CourseDetailActivity.kt
+++ app/src/main/java/com/boostcamp/travery/coursedetail/CourseDetailActivity.kt
@@ -2,7 +2,7 @@ package com.boostcamp.travery.coursedetail
 
 
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.boostcamp.travery.Constants
@@ -10,13 +10,18 @@ import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseActivity
 import com.boostcamp.travery.data.model.Course
 import com.boostcamp.travery.databinding.ActivityCourseDetailBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolylineOptions
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_course_detail.*
 
 
 class CourseDetailActivity : BaseActivity<ActivityCourseDetailBinding>(), OnMapReadyCallback {
-
+    private val compositeDisposable = CompositeDisposable()
     override val layoutResourceId: Int = R.layout.activity_course_detail
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(CourseDetailViewModel::class.java)
@@ -28,14 +33,16 @@ class CourseDetailActivity : BaseActivity<ActivityCourseDetailBinding>(), OnMapR
         super.onCreate(savedInstanceState)
 
         setContentView(viewDataBinding.root)
+        val mapFragment = fragment_map as SupportMapFragment
+        mapFragment.getMapAsync(this)
         setupBindings(savedInstanceState)
     }
 
     private fun setupBindings(savedInstanceState: Bundle?) {
-        val course = intent.getParcelableExtra(Constants.EXTRA_COURSE) as Course
-        Log.e("EE", course.toString())
+        //저장된 것이 없다면 새로 생성된 것으로 간주
+        //intent로 받은 course를 viewmodel로 넘겨 초기화
         savedInstanceState?.let {}
-                ?: viewModel.setCourse(intent.getParcelableExtra(Constants.EXTRA_COURSE))
+                ?: viewModel.init(intent.getParcelableExtra(Constants.EXTRA_COURSE))
         viewDataBinding.viewModel = viewModel
         viewModel.loadUserAtionList()
         //상단 리사이클러뷰 터치 막기
@@ -57,7 +64,11 @@ class CourseDetailActivity : BaseActivity<ActivityCourseDetailBinding>(), OnMapR
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
+        //맵이 준비 되면 viewModel의 LiveData를 observe해서 폴리라인을 그려줌.
+        viewModel.latLng.observe(this, Observer {
+            map.addPolyline(PolylineOptions().addAll(it))
+            map.moveCamera(CameraUpdateFactory.newLatLngBounds(LatLngBounds(it[0], it[it.size - 1]), 0))
+        })
 
     }
 }
--- app/src/main/java/com/boostcamp/travery/coursedetail/CourseDetailViewModel.kt
+++ app/src/main/java/com/boostcamp/travery/coursedetail/CourseDetailViewModel.kt
@@ -1,33 +1,58 @@
 package com.boostcamp.travery.coursedetail
 
 import android.app.Application
-import android.util.Log
+import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.MutableLiveData
 import com.boostcamp.travery.base.BaseViewModel
 import com.boostcamp.travery.data.model.Course
+import com.boostcamp.travery.data.model.TimeCode
+import com.boostcamp.travery.data.model.UserAction
+import com.google.android.gms.maps.model.LatLng
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class CourseDetailViewModel(application: Application) : BaseViewModel(application) {
     private lateinit var course: Course
-    val leftAdapter = UserActionLeftListAdapter()
-    val topAdapter = UserActionTopListAdapter()
+    private val leftActionList = ObservableArrayList<UserAction?>()
+    private val topActionList = ObservableArrayList<UserAction?>()
+    private val timeCodeList = ArrayList<TimeCode>()
+    val latLng = MutableLiveData<List<LatLng>>()
+    val leftAdapter = UserActionLeftListAdapter(leftActionList)
+    val topAdapter = UserActionTopListAdapter(topActionList)
 
+
+    fun init(course: Course) {
+        this.course = course
+        val latLngList = ArrayList<LatLng>()
+        //저장소로부터 TimeCode리스트를 받아 ViewModel의 TimeCode리스트와 LatLng리스트로 저장
+        addDisposable(repository.loadCourseCoordinate(course.startTime.toString())
+                .flatMap { timeList ->
+                    timeCodeList.addAll(timeList)
+                    Flowable.fromIterable(timeList)
+                }
+                .flatMap { timeCode -> Flowable.just(LatLng(timeCode.latitude, timeCode.longitude)) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ latLngList.add(it) }, {}, { latLng.value = latLngList })
+        )
+    }
+
+    //코스에 대한 활동 리스트를 가져옴
     fun loadUserAtionList() {
         addDisposable(repository.getUserActionForCourse(course)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    leftAdapter.addItems(it)
-                    leftAdapter.addItem(null)
-                    leftAdapter.addItem(null)
-                    topAdapter.addItem(null)
-                    topAdapter.addItems(it)
-                    topAdapter.addItem(null)
+                    val arr: Array<UserAction?> = arrayOf(null, null)
+                    leftActionList.addAll(arr)
+                    topActionList.addAll(arr)
+                    topActionList.addAll(1, it)
+                    leftActionList.addAll(0, it)
                 })
     }
 
-    fun setCourse(course: Course) {
-        this.course = course
-    }
 }
+
+
 
