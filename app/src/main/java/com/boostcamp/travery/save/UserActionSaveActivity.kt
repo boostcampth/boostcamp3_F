c7d854656d276f61fedd4c61a3ecd4ffe475787f
onedelay <wjy0218@gmail.com>
2019-02-02 15:43:33 +0900

    (#51) strings.xml 충돌 해결

--- app/src/main/java/com/boostcamp/travery/save/UserActionSaveActivity.kt
+++ app/src/main/java/com/boostcamp/travery/save/UserActionSaveActivity.kt
@@ -1,16 +1,73 @@
 package com.boostcamp.travery.save
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseActivity
 import com.boostcamp.travery.databinding.ActivitySaveUserActionBinding
+import com.boostcamp.travery.mapservice.TrackingActivity
+import com.boostcamp.travery.utils.toast
+import com.esafirm.imagepicker.features.ImagePicker
+import com.tedpark.tedpermission.rx2.TedRx2Permission
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_save_user_action.*
 
-class UserActionSaveActivity : BaseActivity<ActivitySaveUserActionBinding>() {
+class UserActionSaveActivity : BaseActivity<ActivitySaveUserActionBinding>(), UserActionSaveViewModel.Contract {
+    lateinit var viewModel: UserActionSaveViewModel
+    private val compositeDisposable = CompositeDisposable()
+
     override val layoutResourceId: Int
         get() = R.layout.activity_save_user_action
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewDataBinding.root)
+
+//        viewModel = UserActionSaveViewModel(application)
+        viewModel = ViewModelProviders.of(this).get(UserActionSaveViewModel::class.java)
+        viewDataBinding.viewmodel = viewModel
+
+        requestPermission()
+
+        viewModel.setContract(this)
+    }
+
+    private fun requestPermission() {
+        TedRx2Permission.with(this)
+                .setRationaleTitle(getString(R.string.permission_title))
+                .setRationaleMessage(getString(R.string.permission_message_select_image))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .request()
+                .subscribe({ tedPermissionResult ->
+                    if (tedPermissionResult.isGranted) {
+                        startActivity(Intent(this@UserActionSaveActivity, TrackingActivity::class.java))
+                    } else {
+                        getString(R.string.permission_denied) + tedPermissionResult.deniedPermissions.toString().toast(this)
+                    }
+                }, { }).also { compositeDisposable.add(it) }
+    }
+
+    override fun saveSelectedImage() {
+        ImagePicker.create(this)
+                .imageDirectory(getString(R.string.app_name))
+                .single()
+                .start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            ImagePicker.getImages(data).forEach {
+                rv_save_useraction_image_list.scrollToPosition(viewModel.imageList.size)
+                viewModel.imageList.add(viewModel.imageList.size - 1, UserActionImage(it.path))
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/boostcamp/travery/save/UserActionSaveViewModel.kt
+++ app/src/main/java/com/boostcamp/travery/save/UserActionSaveViewModel.kt
@@ -1,9 +1,27 @@
 package com.boostcamp.travery.save
 
 import android.app.Application
+import androidx.databinding.ObservableArrayList
 import com.boostcamp.travery.base.BaseViewModel
 
 class UserActionSaveViewModel(application: Application) : BaseViewModel(application) {
+    val imageList = ObservableArrayList<UserActionImage>()
 
+    private var contract: UserActionSaveViewModel.Contract? = null
 
+    interface Contract {
+        fun saveSelectedImage()
+    }
+
+    fun setContract(contract: Contract) {
+        this.contract = contract
+    }
+
+    fun saveUserAction() {
+        // DB에 저장
+    }
+
+    fun onAddItemClick() {
+        contract?.saveSelectedImage()
+    }
 }
