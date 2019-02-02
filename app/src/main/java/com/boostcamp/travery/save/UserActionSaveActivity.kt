3d9b5e705ed3bce01151c82806598101e7fe3509
onedelay <wjy0218@gmail.com>
2019-02-02 15:39:54 +0900

    (#51) UserAction DB 저장(활동 추가)

--- app/src/main/java/com/boostcamp/travery/save/UserActionSaveActivity.kt
+++ app/src/main/java/com/boostcamp/travery/save/UserActionSaveActivity.kt
@@ -3,11 +3,14 @@ package com.boostcamp.travery.save
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProviders
+import com.boostcamp.travery.Constants
 import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseActivity
 import com.boostcamp.travery.databinding.ActivitySaveUserActionBinding
-import com.boostcamp.travery.mapservice.TrackingActivity
 import com.boostcamp.travery.utils.toast
 import com.esafirm.imagepicker.features.ImagePicker
 import com.tedpark.tedpermission.rx2.TedRx2Permission
@@ -24,6 +27,8 @@ class UserActionSaveActivity : BaseActivity<ActivitySaveUserActionBinding>(), Us
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewDataBinding.root)
+        setSupportActionBar(toolbar as Toolbar)
+        title = ""
 
 //        viewModel = UserActionSaveViewModel(application)
         viewModel = ViewModelProviders.of(this).get(UserActionSaveViewModel::class.java)
@@ -41,12 +46,30 @@ class UserActionSaveActivity : BaseActivity<ActivitySaveUserActionBinding>(), Us
                 .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
                 .request()
                 .subscribe({ tedPermissionResult ->
-                    if (tedPermissionResult.isGranted) {
-                        startActivity(Intent(this@UserActionSaveActivity, TrackingActivity::class.java))
-                    } else {
+                    if (!tedPermissionResult.isGranted) {
                         getString(R.string.permission_denied) + tedPermissionResult.deniedPermissions.toString().toast(this)
                     }
                 }, { }).also { compositeDisposable.add(it) }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_course_add_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+        R.id.menu_course_save -> {
+            with(intent) {
+                viewModel.saveUserAction(
+                        getLongExtra(Constants.EXTRA_LATITUDE, 0),
+                        getLongExtra(Constants.EXTRA_LONGITUDE, 0),
+                        getLongExtra(Constants.EXTRA_COURSE_CODE, 0))
+            }
+            finish()
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
     }
 
     override fun saveSelectedImage() {
--- app/src/main/java/com/boostcamp/travery/save/UserActionSaveViewModel.kt
+++ app/src/main/java/com/boostcamp/travery/save/UserActionSaveViewModel.kt
@@ -3,9 +3,19 @@ package com.boostcamp.travery.save
 import android.app.Application
 import androidx.databinding.ObservableArrayList
 import com.boostcamp.travery.base.BaseViewModel
+import com.boostcamp.travery.data.model.UserAction
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 class UserActionSaveViewModel(application: Application) : BaseViewModel(application) {
     val imageList = ObservableArrayList<UserActionImage>()
+
+    private var title = ""
+        get() = if (field.isEmpty()) "empty" else field
+    private var content = ""
+        get() = if (field.isEmpty()) "empty" else field
+    private var hashTag = ""
+        get() = if (field.isEmpty()) "empty" else field
 
     private var contract: UserActionSaveViewModel.Contract? = null
 
@@ -17,11 +27,38 @@ class UserActionSaveViewModel(application: Application) : BaseViewModel(applicat
         this.contract = contract
     }
 
-    fun saveUserAction() {
+    fun saveUserAction(latitude: Long, longitude: Long, courseCode: Long) {
         // DB에 저장
+        val sb = StringBuilder()
+        imageList.forEach {
+            sb.append(it.filePath + ",")
+        }
+
+        repository.saveUserAction(
+                UserAction(title,
+                        content,
+                        Date(System.currentTimeMillis()),
+                        hashTag,
+                        imageList[0].filePath,
+                        sb.toString(),
+                        latitude, longitude,
+                        courseCode)
+        ).subscribeOn(Schedulers.io()).subscribe().also { addDisposable(it) }
     }
 
     fun onAddItemClick() {
         contract?.saveSelectedImage()
+    }
+
+    fun onTitleChange(title: CharSequence) {
+        this.title = title.toString()
+    }
+
+    fun onContentChange(body: CharSequence) {
+        this.content = body.toString()
+    }
+
+    fun onHashTagChange(body: CharSequence) {
+        this.hashTag = body.toString()
     }
 }
