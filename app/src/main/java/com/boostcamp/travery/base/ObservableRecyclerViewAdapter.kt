3fc29ab2daf2d5cab17fdda1a1a438889c3adc0a
park-ju1008 <p7343703@gmail.com>
2019-02-08 18:09:28 +0900

    (#76) 상단 리사이클러뷰 아이템 안보이는 에러 처리

--- app/src/main/java/com/boostcamp/travery/base/ObservableRecyclerViewAdapter.kt
+++ app/src/main/java/com/boostcamp/travery/base/ObservableRecyclerViewAdapter.kt
@@ -1,11 +1,10 @@
 package com.boostcamp.travery.base
 
-import android.util.Log
 import androidx.databinding.ObservableList
 import androidx.recyclerview.widget.RecyclerView
 
 abstract class ObservableRecyclerViewAdapter<T, Holder : RecyclerView.ViewHolder>(
-    private val items: ObservableList<T>
+        private val items: ObservableList<T>
 ) : RecyclerView.Adapter<Holder>() {
 
     var onItemClickListener: ((item: Any) -> Unit)? = null
@@ -13,32 +12,28 @@ abstract class ObservableRecyclerViewAdapter<T, Holder : RecyclerView.ViewHolder
     init {
         items.addOnListChangedCallback(object : ObservableList.OnListChangedCallback<ObservableList<T>>() {
             override fun onChanged(sender: ObservableList<T>?) {
-                Log.e("ObservableAdapter","Asd")
                 notifyDataSetChanged()
             }
 
             override fun onItemRangeRemoved(sender: ObservableList<T>?, positionStart: Int, itemCount: Int) {
-                Log.e("ObservableAdapter","onItemRangeRemoved")
                 notifyItemRangeRemoved(positionStart, itemCount)
             }
 
             override fun onItemRangeMoved(
-                sender: ObservableList<T>?,
-                fromPosition: Int,
-                toPosition: Int,
-                itemCount: Int
+                    sender: ObservableList<T>?,
+                    fromPosition: Int,
+                    toPosition: Int,
+                    itemCount: Int
             ) {
                 notifyDataSetChanged()
             }
 
             override fun onItemRangeInserted(sender: ObservableList<T>?, positionStart: Int, itemCount: Int) {
-                Log.e("ObservableAdapter","onItemRangeInserted"+positionStart+"/"+itemCount)
-                notifyItemRangeInserted(positionStart,itemCount)
+                notifyItemRangeInserted(positionStart, itemCount)
             }
 
             override fun onItemRangeChanged(sender: ObservableList<T>?, positionStart: Int, itemCount: Int) {
-                Log.e("ObservableAdapter","onItemRangeChanged")
-                notifyItemRangeChanged(positionStart+1, itemCount)
+                notifyItemRangeChanged(positionStart + 1, itemCount)
             }
         })
     }
