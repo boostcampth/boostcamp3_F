7029e9e0f9dd90fb88652c686b16bebe6b8850b6
park-ju1008 <p7343703@gmail.com>
2019-02-09 18:23:25 +0900

    (#91)  커스텀 마커 이미지 보이지 않는 현상 수정

--- app/src/main/java/com/boostcamp/travery/utils/CustomMarker.kt
+++ app/src/main/java/com/boostcamp/travery/utils/CustomMarker.kt
@@ -3,8 +3,11 @@ package com.boostcamp.travery.utils
 import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,18 +15,26 @@ import androidx.annotation.DrawableRes
 import com.boostcamp.travery.GlideApp
 import com.boostcamp.travery.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.ImageViewTarget
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.custom_marker_layout.view.*
+import java.io.File
 
 object CustomMarker {
 
     fun create(context: Context, path: String?): Bitmap {
         val marker = LayoutInflater.from(context).inflate(R.layout.custom_marker_layout, null)
 
-        GlideApp.with(context)
-                .load(path)
-                .error(R.drawable.empty_action_image)
-                .circleCrop()
-                .into(marker.cv_action_image)
+        if(path.isNullOrEmpty()){
+            marker.cv_action_image.setImageResource(R.drawable.empty_action_image)
+        }else{
+            val image=BitmapFactory.decodeFile(File(path).absolutePath)
+            marker.cv_action_image.setImageBitmap(image)
+        }
+
         val displayMetrics = DisplayMetrics()
         (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
         marker.layoutParams = ViewGroup.LayoutParams(52, ViewGroup.LayoutParams.WRAP_CONTENT)
