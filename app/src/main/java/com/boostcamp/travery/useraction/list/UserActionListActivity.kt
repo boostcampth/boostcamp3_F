951d854c7f476cf371b9b264942f4a59c179c489
onedelay <wjy0218@gmail.com>
2019-02-11 01:44:43 +0900

    (#99) 현재 위치에 마커 표시 및 카메라 이동

--- app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListActivity.kt
+++ app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListActivity.kt
@@ -1,6 +1,5 @@
 package com.boostcamp.travery.useraction.list
 
-import android.location.Location
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,6 +14,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_user_action_list.*
 
@@ -23,7 +23,8 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
         get() = R.layout.activity_user_action_list
 
     private var googleMap: GoogleMap? = null
-    private var curLocation: Location? = null
+    private var curLocation: LatLng? = null
+    private var marker: Marker? = null
 
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(UserActionListViewModel::class.java)
@@ -36,17 +37,36 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
 
         viewModel.setContract(this)
 
+        observeCurrentLocation()
+
+        // 현재 위치 표시 및 카메라 이동
+        btn_myLocation.setOnClickListener {
+            marker?.remove()
+            viewModel.setCurrentLocation()
+            curLocation?.let { loc -> moveCamera(loc) }
+        }
+    }
+
+    // 현재 위치 받아오기
+    private fun observeCurrentLocation() {
         viewModel.getCurLocation().observe(this, Observer {
-            curLocation = it
-            // 현재 위치 표시
-            googleMap?.addMarker(MarkerOptions()
-                    .position(LatLng(it.latitude, it.longitude))
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_position_no_heading)))
+            curLocation = LatLng(it.latitude, it.longitude).also { loc ->
+                marker = googleMap?.addMarker(MarkerOptions()
+                        .position(loc)
+                        .flat(true)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_position_no_heading)))
+            }
         })
+    }
+
+    // 해당 위치로 카메라 이동
+    private fun moveCamera(location: LatLng) {
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
     }
 
     override fun onMapReady(map: GoogleMap) {
         this.googleMap = map
+
         viewModel.setCurrentLocation()
         onProgress(resources.getString(R.string.progress_bar_message))
     }
@@ -60,8 +80,9 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
         }
 
         // 초기 화면 위치. 현재 위치를 읽어왔다면 현재위치, 그렇지 않은 경우 첫번째 활동 위치 기준
-        val startPosition = curLocation?.let { LatLng(it.latitude, it.longitude) } ?: LatLng(list[0].latitude, list[0].longitude)
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(startPosition, 15f))
+        val startPosition = curLocation?.let { LatLng(it.latitude, it.longitude) }
+                ?: LatLng(list[0].latitude, list[0].longitude)
+        moveCamera(startPosition)
 
         offProgress()
     }
