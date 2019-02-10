588166ad10425290dd3138f558861120acabf375
onedelay <wjy0218@gmail.com>
2019-02-11 01:06:12 +0900

    (#99) 초기 카메라 위치 설정

--- app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListActivity.kt
+++ app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListActivity.kt
@@ -1,6 +1,8 @@
 package com.boostcamp.travery.useraction.list
 
+import android.location.Location
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseActivity
@@ -21,6 +23,7 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
         get() = R.layout.activity_user_action_list
 
     private var googleMap: GoogleMap? = null
+    private var curLocation: Location? = null
 
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(UserActionListViewModel::class.java)
@@ -32,11 +35,19 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
         (map as SupportMapFragment).getMapAsync(this)
 
         viewModel.setContract(this)
+
+        viewModel.getCurLocation().observe(this, Observer {
+            curLocation = it
+            // 현재 위치 표시
+            googleMap?.addMarker(MarkerOptions()
+                    .position(LatLng(it.latitude, it.longitude))
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_position_no_heading)))
+        })
     }
 
     override fun onMapReady(map: GoogleMap) {
         this.googleMap = map
-
+        viewModel.setCurrentLocation()
         onProgress(resources.getString(R.string.progress_bar_message))
     }
 
@@ -47,7 +58,11 @@ class UserActionListActivity : BaseActivity<ActivityUserActionListBinding>(), On
                     .icon(BitmapDescriptorFactory.fromBitmap(CustomMarker.create(this, it.mainImage))))
                     ?.title = it.title
         }
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(list[0].let { LatLng(it.latitude, it.longitude) }, 15f))
+
+        // 초기 화면 위치. 현재 위치를 읽어왔다면 현재위치, 그렇지 않은 경우 첫번째 활동 위치 기준
+        val startPosition = curLocation?.let { LatLng(it.latitude, it.longitude) } ?: LatLng(list[0].latitude, list[0].longitude)
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(startPosition, 15f))
+
         offProgress()
     }
 }
--- app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListViewModel.kt
+++ app/src/main/java/com/boostcamp/travery/useraction/list/UserActionListViewModel.kt
@@ -1,6 +1,15 @@
 package com.boostcamp.travery.useraction.list
 
+import android.Manifest
 import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.MutableLiveData
+import com.boostcamp.travery.MyApplication
 import com.boostcamp.travery.base.BaseViewModel
 import com.boostcamp.travery.data.model.UserAction
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -8,6 +17,9 @@ import io.reactivex.schedulers.Schedulers
 
 class UserActionListViewModel(application: Application) : BaseViewModel(application) {
     private var contract: Contract? = null
+    private val curLocation = MutableLiveData<Location>()
+
+    fun getCurLocation() = curLocation
 
     init {
         loadUserActions()
@@ -29,4 +41,19 @@ class UserActionListViewModel(application: Application) : BaseViewModel(applicat
                     contract?.onUserActionLoading(it)
                 }.also { addDisposable(it) }
     }
+
+    fun setCurrentLocation() {
+        val fineLocPerm = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+        val courseLocPerm = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        curLocation.value = PackageManager.PERMISSION_GRANTED.let { isGranted ->
+            if (fineLocPerm == isGranted && courseLocPerm == isGranted) {
+                (getContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager).getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            } else {
+                null
+            }
+        }
+    }
+
+    private fun getContext() = getApplication<MyApplication>()
 }
