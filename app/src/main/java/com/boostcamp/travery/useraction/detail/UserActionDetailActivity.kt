ab2ef4ad6e8f8a147ba2f3047c413c387632c176
onedelay <wjy0218@gmail.com>
2019-02-13 23:53:35 +0900

    (#118) 활동 상세화면 수정

--- app/src/main/java/com/boostcamp/travery/useraction/detail/UserActionDetailActivity.kt
+++ app/src/main/java/com/boostcamp/travery/useraction/detail/UserActionDetailActivity.kt
@@ -1,11 +1,15 @@
 package com.boostcamp.travery.useraction.detail
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.boostcamp.travery.Constants
 import com.boostcamp.travery.R
 import com.boostcamp.travery.base.BaseActivity
 import com.boostcamp.travery.databinding.ActivityUserActionDetailBinding
+import kotlinx.android.synthetic.main.activity_user_action_detail.*
 
 class UserActionDetailActivity : BaseActivity<ActivityUserActionDetailBinding>() {
     lateinit var viewModel: UserActionDetailViewModel
@@ -17,9 +21,19 @@ class UserActionDetailActivity : BaseActivity<ActivityUserActionDetailBinding>()
         super.onCreate(savedInstanceState)
         setContentView(viewDataBinding.root)
 
+        setSupportActionBar(toolBar as Toolbar)
+        supportActionBar?.title = ""
+
         viewModel = ViewModelProviders.of(this).get(UserActionDetailViewModel::class.java)
         viewDataBinding.viewmodel = viewModel
 
         viewModel.init(intent.getParcelableExtra(Constants.EXTRA_USER_ACTION))
+
+        LinearSnapHelper().attachToRecyclerView(rv_image_list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_save_edit_menu, menu)
+        return true
     }
 }
