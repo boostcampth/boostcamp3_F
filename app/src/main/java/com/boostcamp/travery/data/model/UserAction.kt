98a828aab1ba90d9a8c2c3c787a2460ac53abb7d
onedelay <wjy0218@gmail.com>
2019-02-15 12:09:39 +0900

    (#117) Room Migration (UserAction 엔티티에 address: String 추가)

--- app/src/main/java/com/boostcamp/travery/data/model/UserAction.kt
+++ app/src/main/java/com/boostcamp/travery/data/model/UserAction.kt
@@ -10,32 +10,33 @@ import java.util.*
 
 @Parcelize
 @Entity(
-    tableName = "useraction",
-    foreignKeys = [ForeignKey(
-        entity = Course::class,
-        parentColumns = ["start_time"],
-        childColumns = ["course_code"],
-        onDelete = ForeignKey.SET_NULL
-    )]
+        tableName = "useraction",
+        foreignKeys = [ForeignKey(
+                entity = Course::class,
+                parentColumns = ["start_time"],
+                childColumns = ["course_code"],
+                onDelete = ForeignKey.SET_NULL
+        )]
 )
 data class UserAction(
-    @ColumnInfo(name = "title") var title: String="",
-    @ColumnInfo(name = "body") var body: String="",
-    @ColumnInfo(name = "date") var date: Date=Date(),
-    @ColumnInfo(name = "hashtag") var hashTag: String="",
-    @ColumnInfo(name = "main_image") var mainImage: String="",
-    @ColumnInfo(name = "sub_image") var subImage: String="",
-    @ColumnInfo(name = "latitude") var latitude: Double,
-    @ColumnInfo(name = "longitude") var longitude: Double,
-    @ColumnInfo(name = "course_code", index = true) var courseCode: Long?=null
+        @ColumnInfo(name = "title") var title: String = "",
+        @ColumnInfo(name = "body") var body: String = "",
+        @ColumnInfo(name = "date") var date: Date = Date(),
+        @ColumnInfo(name = "hashtag") var hashTag: String = "",
+        @ColumnInfo(name = "main_image") var mainImage: String = "",
+        @ColumnInfo(name = "sub_image") var subImage: String = "",
+        @ColumnInfo(name = "latitude") var latitude: Double,
+        @ColumnInfo(name = "longitude") var longitude: Double,
+        @ColumnInfo(name = "course_code", index = true) var courseCode: Long? = null,
+        @ColumnInfo(name = "address") var address: String = " "
 ) : Parcelable {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "seq")
     var seq: Int? = null
 
     override fun equals(other: Any?): Boolean {
-        if(other is UserAction){
-            return this.seq==other.seq
+        if (other is UserAction) {
+            return this.seq == other.seq
         }
         return super.equals(other)
     }
