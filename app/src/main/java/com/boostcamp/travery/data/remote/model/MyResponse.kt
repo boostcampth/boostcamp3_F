2997bac9b70bed9db0d485ed02a20784cc779116
park-ju1008 <p7343703@gmail.com>
2019-02-22 16:57:19 +0900

    (#175) 활동 서버 업로드

--- app/src/main/java/com/boostcamp/travery/data/remote/model/MyResponse.kt
+++ app/src/main/java/com/boostcamp/travery/data/remote/model/MyResponse.kt
@@ -1,6 +1,6 @@
 package com.boostcamp.travery.data.remote.model
 
-data class MyResponse (
-    var error: Boolean = false,
-    var message: String? = null
+data class MyResponse(
+        var error: Boolean = false,
+        var message: String
 )
